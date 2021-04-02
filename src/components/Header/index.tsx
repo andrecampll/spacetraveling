9563de926f49bb29acba791a97aec607c788cfd5
André <andrevictor50@gmail.com>
2021-04-02 17:56:12 -0300

    Adding home structure + styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,11 @@
-export default function Header() {
-  // TODO
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/img/logo.svg" alt="spacetraveling logo" />
+      </div>
+    </header>
+  );
 }
