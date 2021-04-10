046afb1c0bc1b41e5652fdbb379eec12e0bd19a6
André <andrevictor50@gmail.com>
2021-04-10 11:16:44 -0300

    Finishing Challenge

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,14 +1,21 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-return-assign */
 /* eslint-disable react/no-danger */
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Head from 'next/head';
-import Link from 'next/link';
 import { RichText } from 'prismic-dom';
+import Prismic from '@prismicio/client';
+import { Fragment } from 'react';
 import { FiUser, FiCalendar, FiClock } from 'react-icons/fi';
+import { format } from 'date-fns';
+
+import ptBR from 'date-fns/locale/pt-BR';
+import { useRouter } from 'next/router';
 import Header from '../../components/Header';
 
 import { getPrismicClient } from '../../services/prismic';
 
-import commonStyles from '../../styles/common.module.scss';
+// import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 
 interface Post {
@@ -23,7 +30,7 @@ interface Post {
       heading: string;
       body: {
         text: string;
-      };
+      }[];
     }[];
   };
 }
@@ -33,6 +40,23 @@ interface PostProps {
 }
 
 export default function Post({ post }: PostProps): JSX.Element {
+  const router = useRouter();
+
+  const totalWords = post.data.content.reduce((total, contentItem) => {
+    total += contentItem.heading.split(' ').length;
+
+    const words = contentItem.body.map(item => item.text.split(' ').length);
+    words.map(word => (total += word));
+
+    return total;
+  }, 0);
+
+  const readingTime = Math.ceil(totalWords / 200);
+
+  if (router.isFallback) {
+    return <h1>Carregando...</h1>;
+  }
+
   return (
     <>
       <Head>
@@ -49,7 +73,11 @@ export default function Post({ post }: PostProps): JSX.Element {
           <footer>
             <div>
               <FiCalendar size={20} />
-              <time>{post.first_publication_date}</time>
+              <time>
+                {format(new Date(post.first_publication_date), 'PP', {
+                  locale: ptBR,
+                })}
+              </time>
             </div>
             <div>
               <FiUser size={20} />
@@ -57,20 +85,20 @@ export default function Post({ post }: PostProps): JSX.Element {
             </div>
             <div>
               <FiClock size={20} />
-              <span>4 min</span>
+              <span>{`${readingTime} min`}</span>
             </div>
           </footer>
 
           <div className={styles.postContent}>
             {post.data.content.map(content => (
-              <>
+              <Fragment key={content.heading}>
                 <h2>{content.heading}</h2>
                 <div
                   dangerouslySetInnerHTML={{
-                    __html: content.body.text,
+                    __html: RichText.asHtml(content.body),
                   }}
                 />
-              </>
+              </Fragment>
             ))}
           </div>
         </article>
@@ -80,8 +108,22 @@ export default function Post({ post }: PostProps): JSX.Element {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  const postResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      fetch: ['post.title', 'post.subtitle', 'post.author'],
+    }
+  );
+
+  const paths = postResponse.results.map(post => ({
+    params: {
+      slug: post.uid,
+    },
+  }));
+
   return {
-    paths: [],
+    paths,
     fallback: 'blocking',
   };
 };
@@ -96,23 +138,17 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     data: {
       author: response.data.author,
       title: response.data.title,
-      content: response.data.content.map(content => ({
-        heading: content.heading,
-        body: {
-          text: RichText.asHtml(content.body),
-        },
+      subtitle: response.data.subtitle,
+      content: response.data.content.map(item => ({
+        heading: item.heading,
+        body: [...item.body],
       })),
       banner: {
         url: response.data.banner.url,
       },
     },
-    first_publication_date: new Date(
-      response.last_publication_date
-    ).toLocaleDateString('pt-BR', {
-      day: '2-digit',
-      month: 'long',
-      year: 'numeric',
-    }),
+    uid: response.uid,
+    first_publication_date: response.first_publication_date,
   };
 
   return {
